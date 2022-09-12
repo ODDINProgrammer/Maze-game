897efd618fb7a725d97d8d557bed49a2ecc31f33
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2022-09-12 06:14:13 +0300

    Cleaning

--- Assets/Scripts/CustomEditors/CustomMazeEditor.cs
+++ Assets/Scripts/CustomEditors/CustomMazeEditor.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(GenerateMaze))]
-public class CustomMazeEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        if(GUILayout.Button("Generate maze tiles"))
-        {
-            FindObjectOfType<GenerateMaze>().GenerateNewMaze();
-        }
-        if (GUILayout.Button("Delete maze tiles"))
-        {
-            FindObjectOfType<GenerateMaze>().DeleteAllTiles();
-        }
-    }
-}
+//[CustomEditor(typeof(GenerateMaze))]
+//public class CustomMazeEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        if(GUILayout.Button("Generate maze tiles"))
+//        {
+//            FindObjectOfType<GenerateMaze>().GenerateNewMaze();
+//        }
+//        if (GUILayout.Button("Delete maze tiles"))
+//        {
+//            FindObjectOfType<GenerateMaze>().DeleteAllTiles();
+//        }
+//    }
+//}
--- Assets/Scripts/CustomEditors/MinotaurMovementTest.cs
+++ Assets/Scripts/CustomEditors/MinotaurMovementTest.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(BaseMinotaurLogic),true)]
-[CanEditMultipleObjects]
-public class MinotaurMovementTest : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        if (GUILayout.Button("Move minotaur"))
-        {
-            FindObjectOfType<BaseMinotaurLogic>().MovementLogic();
-        }
-    }
-}
+//[CustomEditor(typeof(BaseMinotaurLogic),true)]
+//[CanEditMultipleObjects]
+//public class MinotaurMovementTest : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        if (GUILayout.Button("Move minotaur"))
+//        {
+//            FindObjectOfType<BaseMinotaurLogic>().MovementLogic();
+//        }
+//    }
+//}
